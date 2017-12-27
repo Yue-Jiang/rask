d76c513f0b599ea7bd6acde70032e9a2e99bf57c
Yue-Jiang <rivehill@gmail.com>
2017-12-26 22:59:36 -0800

    preserve error message in package wide environment
    so that i can call ask_stackoverflow() after calling ask_google() for
    the same error message

--- R/rask.R
+++ R/rask.R
@@ -9,12 +9,15 @@
 #' @export
 #' @import utils
 ask_google <- function(keyword = NA, warning = FALSE) {
-    if (is.na(keyword)) {
-        if (warning)
-            keyword <- get_warning() else keyword <- get_error()
+  if (is.na(keyword)) {
+    if (warning) {
+      keyword <- get_warning()
+    } else {
+      keyword <- get_error()
     }
-    keyword <- clean_keyword(keyword)
-    browseURL(sprintf("https://www.google.com/search?q=%s", URLencode(keyword)))
+  }
+  keyword <- clean_keyword(keyword)
+  browseURL(sprintf("https://www.google.com/search?q=%s", URLencode(keyword)))
 }
 
 #' Send search for keyword to Stackoverflow. If no keyword provided, will search on the latest error or warning.
@@ -28,15 +31,17 @@ ask_google <- function(keyword = NA, warning = FALSE) {
 #' @export
 #' @import utils
 ask_stackoverflow <- function(keyword = NA, warning = FALSE) {
-    if (is.na(keyword)) {
-        if (warning)
-            keyword <- get_warning() else keyword <- get_error()
-        keyword <- clean_keyword(keyword)
-        # add an R tag since it came from R session
-        keyword <- paste(keyword, "[R]")
+  if (is.na(keyword)) {
+    if (warning) {
+      keyword <- get_warning()
     } else {
-        keyword <- clean_keyword(keyword)
+      keyword <- get_error()
     }
-    browseURL(sprintf("https://www.stackoverflow.com/search?q=%s", URLencode(keyword)))
+    keyword <- clean_keyword(keyword)
+    # add an R tag since it came from R session
+    keyword <- paste(keyword, "[R]")
+  } else {
+    keyword <- clean_keyword(keyword)
+  }
+  browseURL(sprintf("https://www.stackoverflow.com/search?q=%s", URLencode(keyword)))
 }
-
--- R/utils.R
+++ R/utils.R
@@ -1,19 +1,30 @@
+# cache latest error since it's removed if there had been any successes after the error
+cache_rask_env <- new.env()
+
 get_error <- function() {
-    error_message <- geterrmessage()
+  error_message <- geterrmessage()
+  if (error_message == "subscript out of bounds") {
+    # we have lost the last error due to recent successful executions, so retrieve from cache
+    if (exists("cache_latest_error", envir=cache_rask_env)) {
+      error_message <- get("cache_latest_error", envir=cache_rask_env)
+    } else {
+      error_message <- NA
+    }
+  } else {
+    assign("cache_latest_error", error_message, envir=cache_rask_env)
+  }
+  error_message
 }
 
 get_warning <- function() {
-    last_warning <- tail(warnings(), 1)
-    if (is.null(last_warning))
-        return(NULL)
-    names(last_warning)
+  last_warning <- tail(warnings(), 1)
+  if (is.null(last_warning)) return(NULL)
+  names(last_warning)
 }
 
 clean_keyword <- function(keyword) {
-    # try my best to make this input into a string
-    if (length(keyword) > 1)
-        keyword <- paste(keyword, collapse = " ")
-    if (is.null(keyword) || is.na(keyword))
-        keyword <- ""
-    as.character(keyword)
+  # try my best to make this input into a string
+  if (length(keyword) > 1) keyword <- paste(keyword, collapse = " ")
+  if (is.null(keyword) || is.na(keyword)) keyword <- ""
+  gsub("\n", " ", as.character(keyword))
 }
